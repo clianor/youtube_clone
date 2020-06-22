9f32f590877748e30ec103bec933473e409a7166
clianor <ejrgus94160@gmail.com>
2020-06-23 00:21:53 +0900

    feat: User 모델 및 기본 API 구성
    - User 모델 기본 기능 구현
    - 기본 로그인, 회원가입, 로그아웃 API 구현
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- server/services/auth/routes.js
+++ server/services/auth/routes.js
@@ -1,17 +1,57 @@
-const { request } = require("express");
+const { User } = require("../../models/User");
+const { auth } = require("../../middleware/auth");
 
 const router = require("express").Router();
 
-router.get("/", (req, res, next) => {
-  res.send("auth root");
+router.post("/login", (req, res, next) => {
+  // 로그인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "존재하는 유저가 없습니다.",
+      });
+    }
+
+    // 비밀번호 체크
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
 });
 
-router.get("/login", (req, res, next) => {
-  res.send("login root");
+router.post("/register", (req, res) => {
+  // 회원가입
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
-router.get("/register", (req, res, next) => {
-  res.send("register root");
+router.get("/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
 });
 
 module.exports = router;
