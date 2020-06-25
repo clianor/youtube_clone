f59a618d5f871567bd9ceaee1e32a2d8f6570690
clianor <ejrgus94160@gmail.com>
2020-06-26 00:07:03 +0900

    feat: server 로직과 라우팅 분리
    - 로직과 라우팅을 분리
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- server/services/auth/routes.js
+++ server/services/auth/routes.js
@@ -1,57 +1,22 @@
-const { User } = require("../../models/User");
 const { auth } = require("../../middleware/auth");
+const {
+  loginController,
+  registerController,
+  logoutController,
+} = require("./AuthController");
 
 const router = require("express").Router();
 
-router.post("/login", (req, res, next) => {
-  // 로그인
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        errMsg: "존재하는 유저가 없습니다.",
-      });
-    }
-
-    // 비밀번호 체크
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch)
-        return res.json({
-          loginSuccess: false,
-          errMsg: "비밀번호가 틀렸습니다.",
-        });
-
-      // 토큰 생성
-      user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
-
-        res
-          .cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id });
-      });
-    });
-  });
+router.post("/login", (req, res) => {
+  loginController(req, res);
 });
 
 router.post("/register", (req, res) => {
-  // 회원가입
-  const user = new User(req.body);
-  user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true,
-    });
-  });
+  registerController(req, res);
 });
 
 router.get("/logout", auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).send({
-      success: true,
-    });
-  });
+  logoutController(req, res);
 });
 
 module.exports = router;
