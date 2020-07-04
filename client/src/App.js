fe383fe670d940e723fd34112ed649613b6297a8
clianor <ejrgus94160@gmail.com>
2020-07-05 01:45:11 +0900

    feat: Navbar 추가
    - Navbar 추가
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import CustomRoute from "./components/common/router";
+
+// base components
+import Navbar from "./components/base/Navbar/Navbar";
+
+// pages
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
@@ -8,6 +13,7 @@ import RegisterPage from "./pages/auth/RegisterPage";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <CustomRoute exact path="/" component={LandingPage} isAccess={null} />
         <CustomRoute exact path="/login" component={LoginPage} isAuth={false} />
