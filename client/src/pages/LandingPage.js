61c4f5add6de614968dd845840981af19c137cb9
clianor <ejrgus94160@gmail.com>
2020-07-05 12:10:23 +0900

    refactor: LandingPage 리팩토링
    - 개편
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,36 +1,14 @@
 import React from "react";
-import axios from "axios";
-import { withRouter } from "react-router-dom";
+import { FaCode } from "react-icons/fa";
 
-function LandingPage(props) {
-  const onClickHandler = () => {
-    axios
-      .get(`/api/auth/logout`)
-      .then((res) => {
-        if (res.data.success) {
-          props.history.push("/login");
-        } else {
-          alert("로그아웃에 실패하였습니다.");
-        }
-      })
-      .catch((err) => console.err(err));
-  };
-
+function LandingPage() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <h2>시작 페이지</h2>
-
-      <button onClick={onClickHandler}>로그아웃</button>
+    <div className="CONTENTS">
+      <FaCode style={{ fontSize: "4rem" }} />
+      <br />
+      <span style={{ fontSize: "2rem" }}>Let's Start Coding!</span>
     </div>
   );
 }
 
-export default withRouter(LandingPage);
+export default LandingPage;
