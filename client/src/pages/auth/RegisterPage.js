c582af88e90ae276a17a3297dac517a4a25a88cf
clianor <ejrgus94160@gmail.com>
2020-07-07 00:17:50 +0900

    refactor: 회원가입 리팩토링
    - Formik와 Yup을 활용한 유효성 검사
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- client/src/pages/auth/RegisterPage.js
+++ client/src/pages/auth/RegisterPage.js
@@ -2,99 +2,197 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../../_actions/auth/user";
 import { withRouter } from "react-router-dom";
+import { Form, Typography, Input, Button } from "antd";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import {
+  UserDeleteOutlined,
+  MailOutlined,
+  LockOutlined,
+} from "@ant-design/icons";
+
+const { Title } = Typography;
+const FormItem = Form.Item;
 
 function RegisterPage(props) {
   const dispatch = useDispatch();
 
-  const [Email, setEmail] = useState("");
-  const [Name, setName] = useState("");
-  const [Password, setPassword] = useState("");
-  const [ConfirmPassword, setConfirmPassword] = useState("");
-
-  const onEmailHandler = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const onNameHandler = (event) => {
-    setName(event.target.value);
-  };
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.target.value);
-  };
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-
-    if (Password !== ConfirmPassword) {
-      return alert("비밀번호와 비밀번호 확인이 다릅니다.");
-    }
-
-    const body = {
-      email: Email,
-      name: Name,
-      password: Password,
-    };
-
-    dispatch(registerUser(body)).then((res) => {
-      if (res.payload.success) {
-        props.history.push("/login");
-      } else {
-        alert("Failed to sign up");
-      }
-    });
-  };
+  const [formErrorMessage, setFormErrorMessage] = useState("");
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
+    <Formik
+      initialValues={{
+        email: "",
+        lastName: "",
+        name: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email("Email is invalid")
+          .required("Email is required"),
+        name: Yup.string().required("Name is required"),
+        password: Yup.string()
+          .min(5, "Password must be at least 5 characters")
+          .required("Password is required"),
+        confirmPassword: Yup.string()
+          .oneOf([Yup.ref("password"), null], "Passwords must match")
+          .required("Confirm Password is required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          let dataToSubmit = {
+            email: values.email,
+            name: values.name,
+            password: values.password,
+          };
+
+          dispatch(registerUser(dataToSubmit))
+            .then((res) => {
+              if (res.payload.success) {
+                props.history.push("/login");
+              } else {
+                setFormErrorMessage("Failed to sign up");
+              }
+            })
+            .catch((err) => {
+              setFormErrorMessage("Failed to sign up");
+              setTimeout(() => {
+                setFormErrorMessage("");
+              }, 3000);
+            });
+
+          console.log(dataToSubmit);
+          setSubmitting(false);
+        }, 1000);
       }}
     >
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={onSubmitHandler}
-      >
-        <label htmlFor="REGISTER_EMAIL">Email</label>
-        <input
-          id="REGISTER_EMAIL"
-          type="email"
-          value={Email}
-          onChange={onEmailHandler}
-        />
-        <label htmlFor="REGISTER_NAME">Name</label>
-        <input
-          id="REGISTER_NAME"
-          type="text"
-          value={Name}
-          onChange={onNameHandler}
-        />
-        <label htmlFor="REGISTER_PASSWORD">Password</label>
-        <input
-          id="REGISTER_PASSWORD"
-          type="password"
-          value={Password}
-          onChange={onPasswordHandler}
-        />
-        <label htmlFor="REGISTER_CONFIRM_PASSWORD">Confirm Password</label>
-        <input
-          id="REGISTER_CONFIRM_PASSWORD"
-          type="password"
-          value={ConfirmPassword}
-          onChange={onConfirmPasswordHandler}
-        />
-        <br />
-        <button>회원가입</button>
-      </form>
-    </div>
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <div className="CONTENTS">
+          <Title level={2}>회원가입</Title>
+          <form onSubmit={handleSubmit} style={{ width: "350px" }}>
+            <FormItem required>
+              <Input
+                id="email"
+                prefix={<MailOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Enter your email"
+                type="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.email && touched.email
+                    ? "text-input error"
+                    : "text-input"
+                }
+              />
+              {errors.email && touched.email && (
+                <div className="input-feedback">{errors.email}</div>
+              )}
+            </FormItem>
+
+            <FormItem required>
+              <Input
+                id="name"
+                prefix={
+                  <UserDeleteOutlined style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Enter your name"
+                type="name"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.name && touched.name
+                    ? "text-input error"
+                    : "text-input"
+                }
+              />
+              {errors.name && touched.name && (
+                <div className="input-feedback">{errors.name}</div>
+              )}
+            </FormItem>
+
+            <FormItem required>
+              <Input
+                id="password"
+                prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Enter your password"
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.password && touched.password
+                    ? "text-input error"
+                    : "text-input"
+                }
+              />
+              {errors.password && touched.password && (
+                <div className="input-feedback">{errors.password}</div>
+              )}
+            </FormItem>
+
+            <FormItem required>
+              <Input
+                id="confirmPassword"
+                prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Enter your confirmPassword"
+                type="password"
+                value={values.confirmPassword}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.confirmPassword && touched.confirmPassword
+                    ? "text-input error"
+                    : "text-input"
+                }
+              />
+              {errors.confirmPassword && touched.confirmPassword && (
+                <div className="input-feedback">{errors.confirmPassword}</div>
+              )}
+            </FormItem>
+
+            {formErrorMessage && (
+              <label>
+                <p
+                  style={{
+                    color: "#ff0000bf",
+                    fontSize: "0.7rem",
+                    border: "1px solid",
+                    padding: "1rem",
+                    borderRadius: "10px",
+                  }}
+                >
+                  {formErrorMessage}
+                </p>
+              </label>
+            )}
+
+            <FormItem>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="REGISTER_FROM_BUTTON"
+                style={{ minWidth: "100%" }}
+                disabled={isSubmitting}
+              >
+                회원가입
+              </Button>
+            </FormItem>
+          </form>
+        </div>
+      )}
+    </Formik>
   );
 }
 
