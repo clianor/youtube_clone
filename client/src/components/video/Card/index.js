553d07574c121b8953f613d71557435c67e04014
clianor <ejrgus94160@gmail.com>
2020-07-23 22:11:31 +0900

    refactor: 랜딩페이지 비디오 컴포넌트 분리
    - 비디오 카드로 컴포넌트 별도 분리

---
+++ client/src/components/video/Card/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Col, Card, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+import moment from "moment";
+
+const { Meta } = Card;
+
+function VideCard({ video, minutes, seconds }) {
+  return (
+    <Col xxl={4} xl={6} lg={6} md={8} xs={24} key={video.filePath}>
+      <Card
+        hoverable
+        style={{ width: "100%" }}
+        cover={
+          <div style={{ position: "relative", width: "100%" }}>
+            <img
+              alt={video.title}
+              src={
+                process.env.NODE_ENV === "production"
+                  ? "/" + video.thumbnail
+                  : `http://localhost:5000/${video.thumbnail}`
+              }
+              style={{ width: "100%" }}
+            />
+
+            <div
+              className="VIDEO_DURATION"
+              style={{
+                bottom: 0,
+                right: 0,
+                position: "absolute",
+                color: "#fff",
+                backgroundColor: "rgba(17, 17, 17, 0.8)",
+                opacity: 0.8,
+                width: "100%",
+                textAlign: "right",
+                padding: "2px 4px",
+                borderRadius: "2px",
+                letterSpacing: "0.5px",
+                fontSize: "12px",
+                fontWeight: "500",
+                lineHeight: "12px",
+              }}
+            >
+              <span>
+                {minutes}분 {seconds}초
+              </span>
+            </div>
+          </div>
+        }
+        onClick={() => console.log("card click")}
+      >
+        <Meta
+          avatar={<Avatar icon={<UserOutlined />} />}
+          title={video.title}
+          description={video.writer.name}
+        />
+        <span
+          style={{
+            display: "block",
+            width: "100%",
+            textAlign: "right",
+            marginTop: "0.5rem",
+          }}
+        >
+          조회수 {video.views}회&nbsp;&middot;&nbsp;
+          {moment(video.createdAt).fromNow()}
+        </span>
+      </Card>
+    </Col>
+  );
+}
+
+export default VideCard;
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
-import { Layout, Typography, Col, Row, Card, Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Layout, Typography, Row, Card } from "antd";
 import Axios from "axios";
-import moment from "moment";
+
+import VideoCard from "../components/video/Card";
 
 const { Content } = Layout;
 const { Title } = Typography;
-const { Meta } = Card;
 
 function LandingPage() {
   const [Videos, setVideos] = useState([]);
@@ -26,69 +25,7 @@ function LandingPage() {
     var minutes = Math.floor(video.duration / 60);
     var seconds = Math.floor(video.duration - minutes * 60);
 
-    return (
-      <Col xxl={4} xl={6} lg={6} md={8} xs={24} key={video.filePath}>
-        <Card
-          hoverable
-          style={{ width: "100%" }}
-          cover={
-            <div style={{ position: "relative", width: "100%" }}>
-              <img
-                alt={video.title}
-                src={
-                  process.env.NODE_ENV === "production"
-                    ? "/" + video.thumbnail
-                    : `http://localhost:5000/${video.thumbnail}`
-                }
-                style={{ width: "100%" }}
-              />
-
-              <div
-                className="VIDEO_DURATION"
-                style={{
-                  bottom: 0,
-                  right: 0,
-                  position: "absolute",
-                  color: "#fff",
-                  backgroundColor: "rgba(17, 17, 17, 0.8)",
-                  opacity: 0.8,
-                  width: "100%",
-                  textAlign: "right",
-                  padding: "2px 4px",
-                  borderRadius: "2px",
-                  letterSpacing: "0.5px",
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  lineHeight: "12px",
-                }}
-              >
-                <span>
-                  {minutes}분 {seconds}초
-                </span>
-              </div>
-            </div>
-          }
-          onClick={() => console.log("card click")}
-        >
-          <Meta
-            avatar={<Avatar icon={<UserOutlined />} />}
-            title={video.title}
-            description={video.writer.name}
-          />
-          <span
-            style={{
-              display: "block",
-              width: "100%",
-              textAlign: "right",
-              marginTop: "0.5rem",
-            }}
-          >
-            조회수 {video.views}회&nbsp;&middot;&nbsp;
-            {moment(video.createdAt).fromNow()}
-          </span>
-        </Card>
-      </Col>
-    );
+    return <VideoCard video={video} minutes={minutes} seconds={seconds} />;
   });
 
   return (
